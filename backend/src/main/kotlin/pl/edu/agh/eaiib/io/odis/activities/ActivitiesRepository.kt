66001c90bcf26584da969fa0edc0ca9e7b8890f5
Your Name <you@example.com>
2018-04-28 11:14:58 +0200

    Another failed attempt

--- backend/src/main/kotlin/pl/edu/agh/eaiib/io/odis/activities/ActivitiesRepository.kt
+++ backend/src/main/kotlin/pl/edu/agh/eaiib/io/odis/activities/ActivitiesRepository.kt
@@ -1,6 +1,7 @@
 package pl.edu.agh.eaiib.io.odis.activities
 
 import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.data.mongodb.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
@@ -8,5 +9,6 @@ interface ActivitiesRepository : MongoRepository<NetworkActivity, String>
 {
     fun findActivitiesBySrcAddressHostIpOrDestAddressHostIp(ip1 : String, ip2 : String) : List<NetworkActivity>
     fun findByTimestampBetween(begin: Long, end: Long): List<NetworkActivity>
-    fun findActivitiesByTimestampBetweenAndSrcAddressHostIpOrDestAddressHostIp(begin: Long, end: Long, ip1: String, ip2: String): List<NetworkActivity>
+    @Query("{'srcAddress.host.ip' : '?0', timestamp : {\$gt : ?1, \$lt : ?2}}")
+    fun findActivitesInTimestampRangeWithIp(ip: String, begin: Long, end: Long): List<NetworkActivity>
 }
